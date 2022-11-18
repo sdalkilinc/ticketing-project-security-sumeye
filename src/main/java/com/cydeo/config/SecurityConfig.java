3ad824f1d4b7705b6a83684ae431bc9ea5f3ceea
Sumeye <dalkilincsumeye@gmail.com>
2022-11-17 21:15:32 -0500

    More work added

---
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.cydeo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        List<UserDetails> userList = new ArrayList<>();
+
+        userList.add(
+                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE ADMIN")));
+        )
+
+
+    }
+
+}
