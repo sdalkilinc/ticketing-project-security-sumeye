99a86c1e6b7f8c7db9f374bf0fb4da0822ed684f
Sumeye <dalkilincsumeye@gmail.com>
2022-11-18 16:17:28 -0500

    More work added

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -11,9 +11,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 
     private final SecurityService securityService;
+    private final AuthSuccessHandler authSuccessHandler;
 
-    public SecurityConfig(SecurityService securityService) {
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
         this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
     }
 
 
@@ -59,7 +61,7 @@ public class SecurityConfig {
                 .formLogin()
                     .loginPage("/login")
 //                    .defaultSuccessUrl("/welcome")
-                    .successHandler()
+                    .successHandler(authSuccessHandler)
                     .failureUrl("/login?error=true")
                     .permitAll()
                 .and()
